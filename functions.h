e151bc67db19e8538c3655f2364c6cd091576059
Yelyzaveta Halperina <lizagalperina@gmail.com>
2025-05-08 13:04:56 +0300

    first commit

---
+++ functions.h
@@ -0,0 +1,7 @@
+//
+//  functions.h
+//  project 29.05
+//
+//  Created by Лиза on 08.05.2025.
+//
+
---
+++ project 29.05/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  project 29.05
+//
+//  Created by Лиза on 08.05.2025.
+//
+#include "functions.h"
+#include <iostream>
+
+int main() {
+    
+    
+    return 0;
+}
