5a6274cba0aaca0a46b71e7682c619c356c9f0ac
Yelyzaveta Halperina <lizagalperina@gmail.com>
2025-05-29 00:59:24 +0300

    added handleMenu

--- project 29.05/main.cpp
+++ project 29.05/main.cpp
@@ -28,3 +28,69 @@ struct Task
 // Глобальний масив для зберігання всіх задач та їх кількості
 Task taskList[MAX_TASKS];
 int totalTasks = 0;
+
+bool handleMenu(int choice)
+{
+    if (choice == 1)
+    {
+        addTask();
+    }
+    else if (choice == 2)
+    {
+        deleteTask();
+    }
+    else if (choice == 3)
+    {
+        editTask();
+    }
+    else if (choice == 4)
+    {
+        listTasks();
+    }
+    else if (choice == 5)
+    {
+        listTasksToday();
+    }
+    else if (choice == 6)
+    {
+        listTasksThisWeek();
+    }
+    else if (choice == 7)
+    {
+        listTasksThisMonth();
+    }
+    else if (choice == 8)
+    {
+        searchByTitle();
+    }
+    else if (choice == 9)
+    {
+        searchByPriority();
+    }
+    else if (choice == 10)
+    {
+        searchByDescription();
+    }
+    else if (choice == 11)
+    {
+        sortByPriority();
+    }
+    else if (choice == 12)
+    {
+        sortByDate();
+    }
+    else if (choice == 13)
+    {
+        completeTask();
+    }
+    else if (choice == 0)
+    {
+        cout << "Exiting program. Goodbye!\n";
+        return false; // Повертаємо false, щоб завершити програму
+    }
+    else
+    {
+        cout << "Invalid choice. Please try again.\n";
+    }
+    return true; // Повертаємо true, щоб продовжити роботу програми
+}
