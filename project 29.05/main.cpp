c5795a897cbbc19f134f6e65f95ba6a157cc6568
Olga Sosnovska <moonaileyu@gmail.com>
2025-05-29 00:51:08 +0300

    Добавлены структуры

--- project 29.05/main.cpp
+++ project 29.05/main.cpp
@@ -1,15 +1,30 @@
-//
-//  main.cpp
-//  project 29.05
-//
-//  Created by Лиза on 08.05.2025.
-//
 #include "functions.h"
 #include <iostream>
 using namespace std;
-int main() {
-    
-    cout << "hello";
-    
-    return 0;
-}
+
+// Максимальна кількість задач, яку може зберігати додаток
+const int MAX_TASKS = 100;
+
+// Структура для зберігання дати та часу виконання задачі
+struct DateTime
+{
+    int year;
+    int month;
+    int day;
+    int hour;
+    int minute;
+};
+
+// Структура, що описує одну задачу в списку
+struct Task
+{
+    char title[50];
+    int priority;
+    char description[100];
+    DateTime time;
+    bool completed = false; // Статус задачі: true, якщо виконана, false - якщо ні
+};
+
+// Глобальний масив для зберігання всіх задач та їх кількості
+Task taskList[MAX_TASKS];
+int totalTasks = 0;
